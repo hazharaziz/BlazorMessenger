1223dea47a7a3a8c5ef5e39d4145803b56dfb6a8
Hazhar Aziz <hazharaziz5555@gmail.com>
2020-11-18 20:53:28 +0330

    Implement message repository

---
+++ BlazorMessenger/BlazorMessenger/Interfaces/IMessageRepository.cs
@@ -0,0 +1,20 @@
+﻿using BlazorMessenger.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace BlazorMessenger.Interfaces
+{
+    public interface IMessageRepository
+    {
+        Message Get(int id);
+        IEnumerable<Message> GetAll();
+        IEnumerable<Message> Find(Expression<Func<Message, bool>> predicate);
+        void Add(Message entity);
+        void AddRange(IEnumerable<Message> entities);
+        void Remove(Message entity);
+        void RemoveRange(IEnumerable<Message> entities);
+    }
+}
---
+++ BlazorMessenger/BlazorMessenger/Repositories/UserRepository.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using BlazorMessenger.DataContext;
+using BlazorMessenger.Interfaces;
+using BlazorMessenger.Models;
+
+namespace WebChat.Services
+{
+    public class UserRepository : IUserRepository
+    {
+        public MessengerContext Context;
+
+        public UserRepository(MessengerContext messengerContext)
+        {
+            this.Context = messengerContext;
+        }
+
+        public User Get(int id)
+            => Context.Set<User>().Find(id);
+
+        public IEnumerable<User> GetAll()
+            => Context.Set<User>().ToList();
+
+        public IEnumerable<User> Find(Expression<Func<User, bool>> predicate)
+            => Context.Set<User>().Where(predicate);
+
+        public void Add(User entity)
+            => Context.Set<User>().Add(entity);
+
+        public void AddRange(IEnumerable<User> entities)
+            => Context.Set<User>().AddRange(entities);
+
+        public void Remove(User entity)
+            => Context.Set<User>().Remove(entity);
+
+        public void RemoveRange(IEnumerable<User> entities)
+            => Context.Set<User>().RemoveRange(entities);
+
+        public User GetByUsername(string username)
+            => Find(u => u.Username == username).FirstOrDefault();
+    }
+}
