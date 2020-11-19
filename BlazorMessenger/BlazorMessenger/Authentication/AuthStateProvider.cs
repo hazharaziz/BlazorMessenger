90d294f50274344f71b37f7107ee90deef85ce5e
Hazhar Aziz <hazharaziz5555@gmail.com>
2020-11-19 19:50:04 +0330

    Implement GetAuthenticationStateAsync method

--- BlazorMessenger/BlazorMessenger/Authentication/AuthStateProvider.cs
+++ BlazorMessenger/BlazorMessenger/Authentication/AuthStateProvider.cs
@@ -1,11 +1,21 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Components.Authorization;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 
 namespace BlazorMessenger.Authentication
 {
-    public class AuthStateProvider
+    public class AuthStateProvider : AuthenticationStateProvider
     {
+        public override Task<AuthenticationState> GetAuthenticationStateAsync()
+        {
+            var identity = new ClaimsIdentity(new[] {
+                new Claim(ClaimTypes.Name, "hazhar")
+            });
+            var user = new ClaimsPrincipal(identity);
+            return Task.FromResult(new AuthenticationState(user));
+        }
     }
 }
