271cc6ff924e0400905a368848299af6b9548a83
Hazhar Aziz <hazharaziz5555@gmail.com>
2020-11-19 19:46:15 +0330

    Initialize AuthStateProvider

---
+++ BlazorMessenger/BlazorMessenger/Authentication/AuthStateProvider.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorMessenger.Authentication
+{
+    public class AuthStateProvider
+    {
+    }
+}
