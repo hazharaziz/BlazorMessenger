530941be615dfc676b8c025ee30d2c4b48a302d5
Hazhar Aziz <hazharaziz5555@gmail.com>
2020-11-20 17:17:33 +0330

    Add ILoginAPI

---
+++ BlazorMessenger/BlazorMessenger/Interfaces/ILoginAPI.cs
@@ -0,0 +1,15 @@
+﻿using BlazorMessenger.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorMessenger.Interfaces
+{
+    public interface ILoginAPI
+    {
+        User FetchUser(string username);
+        bool ValidateUserLoginRequest(User user);
+        void AuthenticateUser(User user);
+    }
+}
