74ae4d34fae767c0519b85e608d0aae021f6fce7
Hazhar Aziz <hazharaziz5555@gmail.com>
2020-11-21 08:32:50 +0330

    Add IMessengerAPI

---
+++ BlazorMessenger/BlazorMessenger/Interfaces/IMessengerAPI.cs
@@ -0,0 +1,15 @@
+﻿using BlazorMessenger.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorMessenger.Interfaces
+{
+    public interface IMessengerAPI
+    {
+        List<Message> FetchMessages();
+        void AddMessage(Message message);
+        void EditMessage(int id, string editedMessage);
+    }
+}
