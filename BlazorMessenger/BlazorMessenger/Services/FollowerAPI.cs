0e8a3bbca65e42a577c0c83f163003697c52963c
Hazhar Aziz <hazharaziz5555@gmail.com>
2020-11-22 21:11:37 +0330

    Add FollowerAPI service

---
+++ BlazorMessenger/BlazorMessenger/Services/FollowerAPI.cs
@@ -0,0 +1,18 @@
+﻿using BlazorMessenger.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorMessenger.Services
+{
+    public class FollowerAPI
+    {
+        private IUnitOfWork _unitOfWork;
+
+        public FollowerAPI(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+    }
+}
