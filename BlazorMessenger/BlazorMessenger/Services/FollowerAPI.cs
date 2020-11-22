77bf4eeb0187d11d6a979bd8508e7d87896d68f8
Hazhar Aziz <hazharaziz5555@gmail.com>
2020-11-22 21:23:22 +0330

    Implement IFollowerAPI in FollowerAPI

--- BlazorMessenger/BlazorMessenger/Services/FollowerAPI.cs
+++ BlazorMessenger/BlazorMessenger/Services/FollowerAPI.cs
@@ -1,4 +1,5 @@
 ﻿using BlazorMessenger.Interfaces;
+using BlazorMessenger.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -6,13 +7,25 @@ using System.Threading.Tasks;
 
 namespace BlazorMessenger.Services
 {
-    public class FollowerAPI
+    public class FollowerAPI : IFollowerAPI
     {
         private IUnitOfWork _unitOfWork;
 
         public FollowerAPI(IUnitOfWork unitOfWork)
         {
             _unitOfWork = unitOfWork;
+        }
+
+        public List<User> GetFollowers(int userId)
+        {
+            List<int> followerIds = _unitOfWork.Followers.GetFollowers(userId).Select(f => f.FollowerId).ToList();
+            List<User> followers = new List<User>();
+            foreach (int id in followerIds)
+            {
+                var user = _unitOfWork.Users.Get(id);
+                followers.Add(user);
+            }
+            return followers;
         }
     }
 }
